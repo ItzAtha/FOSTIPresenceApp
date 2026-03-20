3147aed7d68df40763c1fa829a8ee49329ada6b1
ItzAtha <atha.06dha@gmail.com>
2026-03-20 13:38:39 +0700

    Refactor MembersData to support multiple Excel files and improve data loading logic

--- lib/utilities/members_data_factory.dart
+++ lib/utilities/members_data_factory.dart
@@ -7,8 +7,7 @@ import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as path;
 
 class MembersData {
-  late Excel _workbook;
-  final String _fileName;
+  final List<Excel> _workbooks = [];
 
   final List<String> divisionList = [
     "Keilmuan dan Riset Teknologi",
@@ -19,26 +18,33 @@ class MembersData {
     "Keor",
   ];
 
-  MembersData({required String fileName}) : _fileName = fileName;
-
   Future<bool> loadData() async {
+    List<FileSystemEntity> entities = [];
+    List<File> files = [];
+
     Directory? appDocDir = await getExternalStorageDirectory();
-    String filePath = path.join(appDocDir!.path, _fileName);
+    entities = appDocDir != null ? appDocDir.listSync(recursive: true) : [];
 
-    File file = File(filePath);
-    if (!await file.exists()) {
-      print("File not found: $filePath");
+    files = entities.whereType<File>().where((e) => e.path.endsWith(".xlsx")).toList();
+    print(files.map((file) => file.path).toList());
+
+    for (var file in files) {
+      List<int> bytes = await file.readAsBytes();
+      try {
+        Excel workbook = Excel.decodeBytes(bytes);
+        _workbooks.add(workbook);
+        print("Data loaded from ${file.path} | Excel file name: ${path.basename(file.path)}");
+      } catch (e) {
+        print("Error decoding Excel file ${file.path}: $e. Skipping....");
+      }
+    }
+
+    if (_workbooks.isEmpty) {
+      print("No Excel files found in the directory.");
       return false;
     }
 
-    List<int> bytes = await file.readAsBytes();
-    try {
-      _workbook = Excel.decodeBytes(bytes);
-    } catch (e) {
-      print("Error decoding Excel file: $e");
-      return false;
-    }
-    print("Data loaded from $_fileName");
+    print("Total Excel files loaded: ${_workbooks.length}");
     return true;
   }
 
@@ -46,81 +52,108 @@ class MembersData {
     List<CellIndex> startIdColumns = [];
     Map<String, List<List<String>>> studentsDataMap = {};
 
-    if (_workbook.sheets.isEmpty) {
-      print("No sheets found in the workbook.");
-      return studentsDataMap;
-    }
+    for (var workbook in _workbooks) {
+      Map<String, List<List<String>>> workbookDataMap = {};
 
-    Sheet worksheet = _workbook.sheets.values.first;
-    for (var rows in worksheet.rows) {
-      for (var cell in rows) {
-        if (cell == null) continue;
-
-        bool hasMatch = divisionList.any(
-          (division) => StringSimilar.jaccardSimilarity(cell.value.toString(), division) >= 0.8,
-        );
-        if (hasMatch) {
-          print("Found Division on Index ${cell.cellIndex}!");
-          startIdColumns.add(cell.cellIndex);
-          break;
-        }
+      if (workbook.sheets.isEmpty) {
+        print("No sheets found in the workbook.");
+        continue;
       }
-    }
 
-    for (var startIdColumn in startIdColumns) {
-      String divName = worksheet
-          .cell(
+      Iterable<Sheet> worksheets = workbook.sheets.values;
+      for (var worksheet in worksheets) {
+        startIdColumns.clear();
+
+        if (worksheet.rows.isEmpty) {
+          print("No rows found in the worksheet ${worksheet.sheetName}.");
+          continue;
+        }
+
+        for (var rows in worksheet.rows) {
+          for (var cell in rows) {
+            if (cell == null) continue;
+
+            bool hasMatch = divisionList.any(
+                  (division) => StringSimilar.jaccardSimilarity(cell.value.toString(), division) >= 0.8,
+            );
+
+            if (hasMatch) {
+              print("Found Division on Index ${cell.cellIndex} | Value: ${cell.value}");
+              startIdColumns.add(cell.cellIndex);
+              break;
+            }
+          }
+        }
+
+        for (var startIdColumn in startIdColumns) {
+          String divName = worksheet
+              .cell(
             CellIndex.indexByColumnRow(
               columnIndex: startIdColumn.columnIndex,
               rowIndex: startIdColumn.rowIndex,
             ),
           )
-          .value
-          .toString();
-      List<List<String>> studentsData = [];
+              .value
+              .toString();
+          List<List<String>> studentsData = [];
 
-      print("Start ID Column: ${startIdColumn.columnIndex}, Row: ${startIdColumn.rowIndex}");
-      int columnIndex = startIdColumn.columnIndex;
-      int rowIndex = startIdColumn.rowIndex;
+          print("$divName Start ID Column: ${startIdColumn.columnIndex}, Row: ${startIdColumn.rowIndex}");
+          int columnIndex = startIdColumn.columnIndex;
+          int rowIndex = startIdColumn.rowIndex;
 
-      for (int row = rowIndex; row < worksheet.maxRows; row++) {
-        List<String> tempStudentsData = [];
+          for (int row = rowIndex; row < worksheet.maxRows; row++) {
+            List<String> tempStudentsData = [];
 
-        for (int column = columnIndex + 1; column < worksheet.maxColumns - 2; column++) {
-          var cell = worksheet.cell(CellIndex.indexByColumnRow(columnIndex: column, rowIndex: row));
+            for (int column = columnIndex + 1; column < worksheet.maxColumns; column++) {
+              var cell = worksheet.cell(CellIndex.indexByColumnRow(columnIndex: column, rowIndex: row));
 
-          if (cell.value == null || row == rowIndex + 1) continue;
-          print("Cell at Row: $row, Col: $column has value: ${cell.value}");
-          tempStudentsData.add(cell.value.toString());
+              if (cell.value == null || row == rowIndex + 1) continue;
+              print("Cell at Row: $row, Col: $column has value: ${cell.value}");
+              tempStudentsData.add(cell.value.toString());
+            }
+
+            if (tempStudentsData.isNotEmpty) {
+              studentsData.add(tempStudentsData);
+            }
+            print("==================================================================");
+
+            var cell = worksheet.cell(
+              CellIndex.indexByColumnRow(columnIndex: columnIndex, rowIndex: row),
+            );
+            if (cell.value == null) {
+              workbookDataMap[divName] = studentsData;
+              break;
+            }
+          }
         }
+      }
 
-        if (tempStudentsData.isNotEmpty) {
-          studentsData.add(tempStudentsData);
+      print(
+        "===================================================================================================",
+      );
+      workbookDataMap.forEach((key, value) {
+        print("Division: $key");
+        for (var student in value) {
+          print("Student Data: ${student.toString()}");
         }
-        print("==================================================================");
+      });
+      print(
+        "===================================================================================================",
+      );
 
-        var cell = worksheet.cell(
-          CellIndex.indexByColumnRow(columnIndex: columnIndex, rowIndex: row),
-        );
-        if (cell.value == null) {
-          studentsDataMap[divName] = studentsData;
-          break;
-        }
+      if (studentsDataMap.isEmpty) {
+        studentsDataMap = workbookDataMap;
+      } else {
+        workbookDataMap.forEach((key, value) {
+          if (studentsDataMap.containsKey(key)) {
+            studentsDataMap[key]!.addAll(value);
+          } else {
+            studentsDataMap[key] = value;
+          }
+        });
       }
     }
-
-    print(
-      "===================================================================================================",
-    );
-    studentsDataMap.forEach((key, value) {
-      print("Division: $key");
-      for (var student in value) {
-        print("Student Data: ${student.toString()}");
-      }
-    });
-    print(
-      "===================================================================================================",
-    );
+    
     String encodedData = jsonEncode(studentsDataMap);
     print("Encoded Students Data: $encodedData");
     return studentsDataMap;
@@ -153,6 +186,6 @@ class MembersData {
       print("No Student found with NIM: $nim");
     }
 
-    return foundStudent;
+    return foundStudent.sublist(0, 3);
   }
 }
