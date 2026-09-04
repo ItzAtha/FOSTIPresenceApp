8068e1170b47fce925e274c4e7b12809843755d4
ItzAtha <atha.06dha@gmail.com>
2026-09-04 14:12:25 +0700

    Refactor DatabaseManager to use Dio for HTTP requests and improve timeout handling

--- lib/manager/database_manager.dart
+++ lib/manager/database_manager.dart
@@ -1,15 +1,17 @@
 import 'dart:convert';
 
 import 'package:flutter/foundation.dart';
-import 'package:http/http.dart' as http;
-
-import 'events_manager.dart';
-import 'logs_manager.dart';
-import 'members_manager.dart';
+import 'package:dio/dio.dart';
 
 class DatabaseManager {
   final String _baseURL = "https://fostipresensiapi.vercel.app";
+
+  final Dio _dio;
+  final Duration _timeLimit;
+
   String _databaseStatus = 'Idle';
+
+  DatabaseManager({this._timeLimit = const Duration(seconds: 10)}) : _dio = Dio();
 
   Future<bool> createData({
     required String urlPath,
@@ -22,9 +24,11 @@ class DatabaseManager {
 
     try {
       String httpBody = jsonEncode(jsonData);
-      final response = await http
-          .post(url, headers: httpHeaders, body: httpBody)
-          .timeout(Duration(seconds: 10));
+      final response = await _dio.post(
+        url.path,
+        options: Options(headers: httpHeaders),
+        data: httpBody,
+      ).timeout(_timeLimit);
 
       if (response.statusCode == 200 || response.statusCode == 201) {
         _databaseStatus = 'POST ${url.toString()} -> OK (${response.statusCode})';
@@ -57,9 +61,11 @@ class DatabaseManager {
 
     try {
       String httpBody = jsonEncode(jsonData);
-      final response = await http
-          .put(url, headers: httpHeaders, body: httpBody)
-          .timeout(Duration(seconds: 10));
+      final response = await _dio.put(
+        url.path,
+        options: Options(headers: httpHeaders),
+        data: httpBody,
+      ).timeout(_timeLimit);
 
       if (response.statusCode == 200 || response.statusCode == 201) {
         _databaseStatus = 'UPDATE ${url.toString()} -> OK (200)';
@@ -89,7 +95,7 @@ class DatabaseManager {
     bool isSuccess = false;
 
     try {
-      final response = await http.delete(url).timeout(Duration(seconds: 10));
+      final response = await _dio.delete(url.path).timeout(_timeLimit);
       if (response.statusCode == 200) {
         _databaseStatus = 'DELETE ${url.toString()} -> OK (200)';
         isSuccess = true;
@@ -109,30 +115,21 @@ class DatabaseManager {
     return isSuccess;
   }
 
-  Future<dynamic> readData({
-    required String urlPath,
-    String dataId = "",
+  Future<Map<String, dynamic>> readData({
+    required String endpoint,
+    String? dataId,
     bool onlyReadLogs = false,
     bool showLogs = false,
   }) async {
-    Uri url = Uri.parse("$_baseURL/$urlPath/$dataId");
-    dynamic dataList;
+    Uri url = Uri.parse("$_baseURL/$endpoint/${dataId ?? ''}");
+    Map<String, dynamic> responseResult = {};
 
     try {
-      final response = await http.get(url).timeout(Duration(seconds: 10));
-      final responseBody = response.body;
+      final response = await _dio.get(url.toString()).timeout(_timeLimit);
+      final responseBody = response.data as Map<String, dynamic>;
 
       if (response.statusCode == 200) {
-        if (urlPath.endsWith('mahasiswa')) {
-          dataList = parseUser(responseBody);
-        } else if (urlPath.endsWith('event')) {
-          if (onlyReadLogs && dataId != "") {
-            dataList = parseLogs(responseBody);
-          } else {
-            dataList = parseEvent(responseBody);
-          }
-        }
-
+        responseResult = responseBody;
         _databaseStatus = 'GET ${url.toString()} -> OK (200)';
       } else if (response.statusCode == 404 ||
           (response.statusCode == 200 && responseBody.isEmpty)) {
@@ -150,6 +147,6 @@ class DatabaseManager {
       print(_databaseStatus);
     }
 
-    return dataList;
+    return responseResult;
   }
 }
