abd268a74bcd859b781d984e332e40716dd4885f
ItzAtha <atha.06dha@gmail.com>
2026-09-04 17:06:52 +0700

    Add AppSizes to define application size constants for padding, text, and rounded elements

---
+++ lib/core/constants/app_sizes.dart
@@ -0,0 +1,24 @@
+class AppSizes {
+  AppSizes._();
+
+  // Padding & Margin size
+  static const double p8 = 8.0;
+  static const double p12 = 12.0;
+  static const double p16 = 16.0;
+  static const double p24 = 24.0;
+  static const double p32 = 32.0;
+
+  // Text size
+  static const double sTextSize = 12.0;
+  static const double smTextSize = 14.0;
+  static const double mTextSize = 16.0;
+  static const double mlTextSize = 18.0;
+  static const double lTextSize = 20.0;
+  static const double xlTextSize = 24.0;
+  static const double xxlTextSize = 28.0;
+
+  // Rounded size
+  static const double smallRounded = 8.0;
+  static const double mediumRounded = 16.0;
+  static const double largeRounded = 32.0;
+}
