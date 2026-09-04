097fc62bc210ddcf617b984bc893365bb2d9d659
ItzAtha <atha.06dha@gmail.com>
2026-09-04 17:01:47 +0700

    Add AppColors class to define color constants for the application

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  // Based app color
+  static const Color primary = Color(0xFF0EA253);
+  static const Color secondary = Color(0xFF00CEC9);
+
+  // Background app color
+  static const Color bgLight = Color(0xFFF5F6FA);
+  static const Color bgDark = Color(0xFF212121);
+
+  // Card color
+  static const Color cardLight = Color(0xFFFCFBFB);
+  static const Color cardDark = Color(0xFF28282B);
+
+  // Text & Icons color
+  static const Color textLight = Color(0xFF1C1C1E);
+  static const Color textDark = Color(0xFFE0E0E0);
+
+  static const Color iconLight = Color(0xFF3A3A3C);
+  static const Color iconDark = Color(0xFF8E8E93);
+
+  // Danger zone color
+  static const Color dangerZone = Colors.redAccent;
+}
