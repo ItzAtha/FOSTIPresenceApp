449868e6c48288fb6b78da3629e4d56c9b2e64fe
ItzAtha <atha.06dha@gmail.com>
2026-09-04 20:30:43 +0700

    Add floating action button with multiple actions to BasePages

--- lib/features/dashboard/base_pages.dart
+++ lib/features/dashboard/base_pages.dart
@@ -1,10 +1,13 @@
 import 'package:attendance_management/features/dashboard/widgets/navbar_widget.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:go_router/go_router.dart';
 import 'package:material_ui/material_ui.dart';
 import 'package:toastification/toastification.dart';
 
+import '../../core/constants/app_colors.dart';
+import '../../core/constants/app_sizes.dart';
 import '../../translations/locale_keys.g.dart';
 
 class BasePages extends StatefulWidget {
@@ -57,6 +60,56 @@ class _BasePagesState extends State<BasePages> {
               initialLocation: index == widget._navigationShell.currentIndex,
             );
           },
+          floatingActionButton: SpeedDial(
+            elevation: 2.0,
+            spacing: 3.0,
+            icon: Icons.add,
+            activeIcon: Icons.close,
+            foregroundColor: Colors.white,
+            backgroundColor: AppColors.primary,
+            childPadding: const EdgeInsets.all(AppSizes.p8 / 2),
+            spaceBetweenChildren: 4.0,
+            overlayColor: Colors.black,
+            overlayOpacity: 0.5,
+            labelTransitionBuilder: (widget, animation) =>
+                ScaleTransition(scale: animation, child: widget),
+            animationDuration: const Duration(milliseconds: 300),
+            children: [
+              SpeedDialChild(
+                child: const FaIcon(FontAwesomeIcons.creditCard, size: 25.0),
+                backgroundColor: Colors.blueAccent,
+                foregroundColor: Colors.white,
+                label: 'Attendance',
+                labelStyle: Theme.of(context).textTheme.labelMedium,
+                labelBackgroundColor: Theme.of(context).cardTheme.color,
+                onTap: () {
+                  // Handle add action
+                },
+              ),
+              SpeedDialChild(
+                child: const FaIcon(FontAwesomeIcons.userPlus, size: 20.0),
+                backgroundColor: Colors.cyan,
+                foregroundColor: Colors.white,
+                label: 'Add Member',
+                labelStyle: Theme.of(context).textTheme.labelMedium,
+                labelBackgroundColor: Theme.of(context).cardTheme.color,
+                onTap: () {
+                  // Handle add action
+                },
+              ),
+              SpeedDialChild(
+                child: const FaIcon(FontAwesomeIcons.calendarPlus, size: 25.0),
+                backgroundColor: Colors.orangeAccent,
+                foregroundColor: Colors.white,
+                label: 'Add Event',
+                labelStyle: Theme.of(context).textTheme.labelMedium,
+                labelBackgroundColor: Theme.of(context).cardTheme.color,
+                onTap: () {
+                  // Handle add action
+                },
+              ),
+            ],
+          ),
           destinations: [
             const DestinationNavbarWidget(label: 'Home', icon: FaIcon(FontAwesomeIcons.solidHouse)),
             const DestinationNavbarWidget(label: 'Members', icon: FaIcon(FontAwesomeIcons.users)),
