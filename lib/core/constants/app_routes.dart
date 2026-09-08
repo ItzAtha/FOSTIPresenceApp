3c1de949f09dc6dfda2e1422bb31cc73e536f257
ItzAtha <atha.06dha@gmail.com>
2026-09-08 09:22:07 +0700

    Add new action button routes for adding members and managing attendance

--- lib/core/constants/app_routes.dart
+++ lib/core/constants/app_routes.dart
@@ -6,4 +6,9 @@ class AppRoutes {
   static const ({String name, String path}) memberRoute = (path: '/members', name: "members");
   static const ({String name, String path}) eventRoute = (path: '/events', name: "events");
   static const ({String name, String path}) settingRoute = (path: '/setting', name: "settings");
+
+  // ============[ Action button routes ]============
+  static const ({String name, String path}) addMemberRoute = (path: 'add-member', name: "add-member");
+  static const ({String name, String path}) bluetoothMenuRoute = (path: 'bluetooth-menu', name: "bluetooth-menu");
+  static const ({String name, String path}) memberAttendanceRoute = (path: 'member-presence', name: "member-presence");
 }
