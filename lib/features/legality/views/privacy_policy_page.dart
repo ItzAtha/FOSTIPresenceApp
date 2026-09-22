2bbf662af97073a686389a8e77cafb19d893b910
ItzAtha <atha.06dha@gmail.com>
2026-09-22 08:58:53 +0700

    Add Privacy Policy and Terms of Service pages with markdown support

---
+++ lib/features/legality/views/privacy_policy_page.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:flutter_markdown_plus/flutter_markdown_plus.dart';
+import 'package:go_router/go_router.dart';
+import 'package:material_ui/material_ui.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import '../../../core/app_constants.dart';
+
+class PrivacyPolicyPage extends StatelessWidget {
+  const PrivacyPolicyPage({super.key});
+
+  Future<String> _loadPrivacyPolicy() async {
+    return await rootBundle.loadString('assets/legality/privacy_policy.md');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Privacy Policy'),
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+        leading: BackButton(
+          style: const ButtonStyle(
+            backgroundColor: WidgetStatePropertyAll<Color>(Colors.transparent),
+          ),
+          onPressed: () => context.pop(),
+        ),
+      ),
+      body: SafeArea(
+        child: FutureBuilder<String>(
+          future: _loadPrivacyPolicy(),
+          builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text(
+                  'Error loading privacy policy: \n${snapshot.error}',
+                  textAlign: TextAlign.center,
+                ),
+              );
+            } else {
+              return Scrollbar(
+                child: Markdown(
+                  data: snapshot.data ?? 'No privacy policy available.',
+                  padding: const EdgeInsets.all(AppSizes.p16),
+                  onTapLink: (text, url, title) {
+                    if (url != null) {
+                      launchUrl(Uri.parse(url));
+                    }
+                  },
+                  styleSheet: MarkdownStyleSheet(
+                    p: Theme.of(context).textTheme.labelMedium?.copyWith(height: 1.5),
+                    h1: Theme.of(context).textTheme.titleLarge,
+                    h2: Theme.of(context).textTheme.titleMedium,
+                    listBullet: Theme.of(context).textTheme.labelMedium?.copyWith(height: 1.5),
+                  ),
+                ),
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
